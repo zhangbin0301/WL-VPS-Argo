015edff0c2d4a973d64b630f22119f670454ce67
zhangbin0301 <zhangbin0301@qq.com>
2025-12-29 16:24:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -362,9 +362,8 @@ install_naray(){
         fi
         CF_IP=${CF_IP:-"ip.sb"}
         echo -e -n "${GREEN}节点上传TG,格式: "CHAT_ID BOT_TOKEN（558914831 5824972634:AAGJG-FBAgPljwpnlnD8Lk5Pm2r1QbSk1AI ）": ${PLAIN}"
-        read CHAT_ID BOT_TOKEN
-        CHAT_ID BOT_TOKEN=${CHAT_ID BOT_TOKEN:-"558914831 5824972634:AAGJG-FBAgPljwpnlnD8Lk5Pm2r1QbSk1AI"}
         read TG
+        CHAT_ID TG=${TG:-"558914831 5824972634:AAGJG-FBAgPljwpnlnD8Lk5Pm2r1QbSk1AI"}        
     }
 
     install_config2(){
